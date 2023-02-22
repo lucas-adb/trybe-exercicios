3e9279df9f23ad734d7c1b958d0fef7aa87fe57d
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-02-22 13:49:29 -0300

    fundamentos-3.1 exercicio 9 resolucao

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicios/exercicio-9/script.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 20;
+let c = 33;
+
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    console.log(true);
+} else {
+    console.log(false);
+};
+
+// if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+//     console.log(false);
+// } else {
+//     console.log(true);
+// };
