bc14156e55858c864a4e0b9f46c86194b2bef843
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-02-24 16:42:22 -0300

    fundamentos-3.2 - exercicio bonus 4 resolucao

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios/exercicio-bonus-4/script.js
@@ -0,0 +1,11 @@
+let valor = 5;
+let asteriscos = "";
+let asterisco = "*"; 
+
+if (valor > 1) {
+    for (let index = 0; index < valor; index += 1) {
+        asteriscos += asterisco;
+    }   for (let index = 0; index < valor; index += 1) {
+        console.log(asteriscos);
+    } 
+}
