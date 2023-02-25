0dab4b65ce63343192a6604ed76e3c313e43754e
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-02-25 13:25:58 -0300

    fundamentos-3.2 - exercicio bonus 6 resolucao

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios/exercicio-bonus-6/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios/exercicio-bonus-6/script.js
@@ -1,10 +1,29 @@
 let valor = 5;
-let asteriscos = "";
-let asterisco = "*"; 
+let linha = "";
+let asterisco = "*";
+let espaco = " ";
+// let posicao = valor - 1;
 
-if (valor > 1) {
-    for (let index = valor; index > 0; index -= 1) {
-        asteriscos += asterisco;
-        console.log(asteriscos);
-    }
+
+// for (let index = 0; index < valor; index += 1) {
+//     linha += asterisco;
+//     console.log(linha);
+// }
+
+// for (let index = 0; index < valor; index += 1) {
+//     for (let index2 = 0; index2 < valor; index2 += 1) {
+//         if (index2 < posicao) {
+//             linha = linha + " ";
+//         } else {
+//             linha = linha + "*";
+//         }
+//     }
+//     console.log(linha);
+//     linha = "";
+//     posicao -= 1;
+// }
+
+for (let index = 1; index <= valor; index += 1) {
+    linha = espaco.repeat(valor - index) + asterisco.repeat(index);
+    console.log(linha);
 }
