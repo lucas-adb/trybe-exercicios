9e04a8127073691baa0cd76c129c465cab7f25cb
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-03-05 19:04:57 -0300

    exercicio parte-1.3 resolucao

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
@@ -9,12 +9,18 @@
 
 // #2
 
-const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-  pessoa.nome = 'Luna';
-  pessoa.idade = 19;
+// const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa.nome = 'Luna';
+//   pessoa.idade = 19;
 
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+  // #3
+
+  let favoriteFood = 'Lasanha';
+  favoriteFood = 'Hambúrguer';
+  console.log(favoriteFood);
