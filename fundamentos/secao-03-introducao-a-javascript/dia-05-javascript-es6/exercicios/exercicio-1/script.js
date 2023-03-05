a7037aaa829d999d8553a5f58a009f9add54ce2c
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-03-05 19:08:41 -0300

    exercicio parte-1.3 resolucao

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
@@ -21,6 +21,19 @@
 
   // #3
 
-  let favoriteFood = 'Lasanha';
-  favoriteFood = 'Hambúrguer';
-  console.log(favoriteFood);
+//   let favoriteFood = 'Lasanha';
+//   favoriteFood = 'Hambúrguer';
+//   console.log(favoriteFood);
+
+  // #4
+
+  const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá, ${name} ${lastName}!`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} é: ${a + b}`);
