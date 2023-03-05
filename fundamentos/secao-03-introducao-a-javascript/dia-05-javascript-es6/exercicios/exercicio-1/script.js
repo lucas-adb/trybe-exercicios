ef5920c06932f621b7fb220bc08e0ae9f4b3d708
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-03-05 19:22:34 -0300

    exercicio parte-1.8 resolucao

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
@@ -52,7 +52,14 @@
 
 // #7
 
-  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
-  let nome = 'Ivan';
-  let sobrenome = 'Pires';
-  console.log(nomeCompleto(nome, sobrenome));
+//   const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+// #8 
+
+let speed = 90;
+
+const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`
+console.log(speedCar(speed));
