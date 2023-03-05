9508aa3c2ee4b4aa260f6276f27d7cc535a171dd
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-03-05 19:03:42 -0300

    exercicio parte-1.2 resolucao

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicios/exercicio-1/script.js
@@ -1,7 +1,20 @@
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
-  }
-  imprimeIdade()
+// #1
 
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+//   }
+//   imprimeIdade()
+
+// #2
+
+const pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa.nome = 'Luna';
+  pessoa.idade = 19;
+
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
